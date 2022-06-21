da63376dfd78fd37cbb4a22eb4b0856baa5ab7a6
Abdulfatai Quadri Adewale <75619825+abujafar93@users.noreply.github.com>
2022-06-22 00:46:29 +0100

    update script file

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const clock = document.querySelector('.clock');
 const dateToday = document.querySelector('.date');
-const now = new Date();
 
 const tick = () => {
 
-  
+  const now = new Date();  
 
   const hour = now.getHours();
   const min = now.getMinutes();
@@ -12,14 +11,14 @@ const tick = () => {
 
   const html = `<span>${hour}</span> : <span>${min}</span> : <span>${second}</span>`;
 
-  // html.style.color = white;
-
   clock.innerHTML = html;
 }
 
-setInterval(tick, 1000)
+setInterval(tick, 1000);
+
 
 const dateNow = () => {
+  const now = new Date();  
   
   const todayDate = dateFns.format(now, 'dddd, MMMM Do YYYY');
 
